873440c19fb2e686770edde299ce5a3ae4d063b8
Harini <harinik030@gmail.com>
2026-03-10 20:43:06 +0530

    first commit

---
+++ src/Excercise/Amazon.java
@@ -0,0 +1,15 @@
+package Excercise;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get()
+		
+
+	}
+
+}
